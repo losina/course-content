7e652c9fcc0aeec4924ec937e8b139d1a59d9140
Spiros Ch <spirosChv@users.noreply.github.com>
2021-02-01 12:30:19 +0200

    Update W2_Tutorial2_Solution_Ex08.py

--- tutorials/W2_PyTorchDLN/solutions/W2_Tutorial2_Solution_Ex08.py
+++ tutorials/W2_PyTorchDLN/solutions/W2_Tutorial2_Solution_Ex08.py
@@ -1,11 +1,10 @@
-def plot_distances(sample_size):
+def plot_distances(sample_size, dimensions):
 
-  cnt = 0
   plt.figure(figsize=(15, 12))
-  for D in [1, 2, 5, 10, 100, 1000]:
-    cnt += 1
+  for cnt, D in enumerate(dimensions):
+
     norms = []
-        
+
     if D != 1:
       mean = np.zeros(D)
       cov = 2*np.eye(D)  # diagonal covariance, i.e., isotropic gaussian
@@ -20,9 +19,9 @@ def plot_distances(sample_size):
       y = np.random.randn(sample_size)
       norms = np.abs(x - y)
     
-    if cnt == 1:
+    if cnt == 0:
       bins = np.histogram(norms, bins=100)[1]  # get the bin edges
-    plt.subplot(3,2,cnt)
+    plt.subplot(3, 2, cnt + 1)
     plt.hist(norms, bins=bins, density=True, alpha=0.5)
     if D == 1:
       plt.xlabel('|x-y|')
@@ -36,4 +35,5 @@ def plot_distances(sample_size):
 
 
 sample_size = 500
+dimensions = [1, 2, 5, 10, 100, 1000]
 plot_distances(sample_size)

25bb7b921f9052a65b180be48748250d7577cd7b
Spiros Ch <spirosChv@users.noreply.github.com>
2021-02-01 13:33:29 +0200

    Update W2_Tutorial2_Solution_Ex05.py

--- tutorials/W2_PyTorchDLN/solutions/W2_Tutorial2_Solution_Ex05.py
+++ tutorials/W2_PyTorchDLN/solutions/W2_Tutorial2_Solution_Ex05.py
@@ -2,27 +2,16 @@ def training_modes(inputs, targets):
 
   input_dim = inputs.shape[1]
   output_dim = targets.shape[1]
-  hidden = 20
+  hidden_sizes = 20
 
-  # Initial Weights and biases
-  sigma = 0.001
-  w1 = Variable(torch.mul(torch.randn(hidden, input_dim), sigma),
-                requires_grad=True)
-  b1 = Variable(torch.mul(torch.randn(hidden), 0.0),
-                requires_grad=True)
-
-  w2 = Variable(torch.mul(torch.randn(output_dim, hidden), sigma),
-                requires_grad=True)
-  b2 = Variable(torch.mul(torch.randn(output_dim), 0.0),
-                requires_grad=True)
-
-  params = [w1, b1, w2, b2]
-
-  lr  = 2e-3  # learning rate
+  learning_rate  = 2e-3  # learning rate
   num_epochs = 3000
 
   # Loss function
   criterion = nn.MSELoss()
+
+  model = networkSVD(input_dim, output_dim, hidden_sizes)
+  optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
   # Train for num_epochs
   losses = []
@@ -37,29 +26,21 @@ def training_modes(inputs, targets):
       epoch_range.refresh() # to show immediately the update
     time.sleep(0.01)
 
-    preds = modelSVD(inputs, params)
+    preds = model(inputs)
     
     loss = criterion(preds, targets)
     loss.backward()
 
     # SVD applied on the matrix product
-    w_mult = (w1.T @ w2.T).detach().numpy()
+    w_mult = (model[0].weight.T @ model[1].weight.T).detach().numpy()
     w_svd = np.linalg.svd(w_mult, compute_uv=False, full_matrices=True)
     modes[:,epoch] = w_svd[:rank]
-    # Adjust weights & reset gradients
-    with torch.no_grad():
-      # Gradient descent
-      w1 -= w1.grad * lr
-      b1 -= b1.grad * lr
-      w2 -= w2.grad * lr
-      b2 -= b2.grad * lr
-      # flush fradients
-      w1.grad.zero_()
-      b1.grad.zero_()
-      w2.grad.zero_()
-      b2.grad.zero_()
+
+    # Grdient descent
+    optimizer.step()
+    optimizer.zero_grad()
         
-    losses.append(loss.detach().numpy())
+    losses.append(loss)
 
   return (losses, modes, num_epochs)
 

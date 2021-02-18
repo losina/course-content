6f6d62dba57a260d2331703628fe2303573fe144
RaviKonkimalla <ravi.teja.konkimalla@gmail.com>
2021-02-18 18:19:26 -0500

    Update W5_Tutorial1_Ex02.py

--- tutorials/W5_Regularization/solutions/W5_Tutorial1_Ex02.py
+++ tutorials/W5_Regularization/solutions/W5_Tutorial1_Ex02.py
@@ -7,9 +7,15 @@ def Visualize_data(dataloader):
   for idx,(data,label) in enumerate(dataloader):
 
     plt.figure(idx)
+    #Choose the datapoint you would like to visualize
     index = 22
+    
+    #choose that datapoint using index and permute the dimensions and bring the pixel values between [0,1]
     data = data[index].permute(1,2,0)* torch.tensor([0.5,0.5,0.5]) + torch.tensor([0.5,0.5,0.5])
+    
+    #Convert the torch tensor into numpy
     data = data.numpy()
+    
     plt.imshow(data)
     image_class = classes[label[index].item()]
     print(f'The image belongs to : {image_class}')
